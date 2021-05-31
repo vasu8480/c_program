cfa6ba40869422aab4d4746ba84b2cb83dbfeb88
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2021-05-31 15:45:34 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i ,marks[400],s;
+	scanf("%d",&s);
+	for(i=0;i<s;i++)
+	{
+		scanf("%d",&marks[i]);
+		
+	}
+	for(i=0;i<s;i++)
+	{
+		printf("%d",marks[i]);
+		
+	}
+	return 0;
+}
